6d448a0760c786b4106b044e2cd2f3f6ef5f3884
roxeannechico <roseannechico22@gmail.com>
2024-10-19 19:05:47 +0800

    COMMIT_EDITMSG

---
+++ arrayActivity.js
@@ -0,0 +1,8 @@
+// Favorite Singers
+let favSingers = ["Taylor Swift", "Lany", "TJ Monterde"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["09", "22", "24", "05"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr);
